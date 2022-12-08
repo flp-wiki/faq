9dd32fa7465b96e8f4d58b872a287eb31fcb5e22
Patrick Cunningham <pcunning@users.noreply.github.com>
2022-12-07 16:48:00 -0800

    add question

--- main.go
+++ main.go
@@ -36,6 +36,12 @@ func main() {
 			Answer:   "Some card readers operate at slightly different frequencies and the flipper cant detect that like a card.",
 			Redirect: "https://github.com/djsime1/awesome-flipperzero/blob/main/FAQ.md#why-isnt-mifare-classic-emulation-working",
 		},
+		{
+			Path:     "bettse",
+			Question: "Why is bettse a jerk?",
+			Answer:   "He's not a jerk, he's just a little misunderstood.",
+			Redirect: "https://ericbetts.org",
+		},
 	}
 
 	pageTemplate := template.Must(template.ParseFiles("template.html"))
