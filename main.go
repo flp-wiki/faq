907e80f37323928b1fff040ee40be905510eb858
Eric Betts <bettse@fastmail.fm>
2022-12-07 20:29:21 -0800

    more faq

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 		{
 			Path:     "nfc/mfc-emulation",
 			Question: "Why isn't Mifare Classic emulation working?",
-			Answer:   "Some card readers operate at slightly different frequencies and the flipper cant detect that like a card.",
+			Answer:   "Some card readers operate at slightly different frequencies and the flipper can't detect that like a card.",
 			Redirect: "https://github.com/djsime1/awesome-flipperzero/blob/main/FAQ.md#why-isnt-mifare-classic-emulation-working",
 		},
 		{
@@ -41,6 +41,24 @@ func main() {
 			Question: "Why is bettse a jerk?",
 			Answer:   "He's not a jerk, he's just a little misunderstood.",
 			Redirect: "https://ericbetts.org",
+		},
+		{
+			Path:     "nfc/mfkey",
+			Question: "What is mfkey32v2?",
+			Answer:   "A way to use math to recover Mifare Classic keys from a reader.",
+			Redirect: "https://github.com/equipter/mfkey32v2#mfkey32-version-2",
+		},
+		{
+			Path:     "nfc/dict/stuck",
+			Question: "Mifare Classic read is stuck!",
+			Answer:   "The LED is blinking, it is still working.  It has a lot of keys to test.",
+			Redirect: "https://github.com/djsime1/awesome-flipperzero/blob/main/FAQ.md#why-does-it-take-so-long-to-read-a-mifare-classic",
+		},
+		{
+			Path:     "nfc/support",
+			Question: "Why doesn't flipper support NFC-B/NFC-F/NFC-V/NTAG Write/other?",
+			Answer:   "The Flipper team is not large, and they focus on the featuers they thing are most popular first",
+			Redirect: "https://docs.flipperzero.one/",
 		},
 	}
 
